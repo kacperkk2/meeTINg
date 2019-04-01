13c22a9bf19d6f7e0ee336198fe1d38b3cd46337
tsuchodolskij <T.Suchodolskij@stud.elka.pw.edu.pl>
2019-04-02 01:08:17 +0200

    login view

--- meeting-client-application/src/main/java/Main.java
+++ meeting-client-application/src/main/java/Main.java
@@ -14,14 +14,16 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
         Stage loginStage = new Stage();
 
         loadStage(loginStage, "/fxml/LoginWindow.fxml", "meeTINg Client Application");
         loginStage.show();
-        loginStage.setX((primScreenBounds.getWidth()/2 - loginStage.getWidth()) / 2);
-        loginStage.setY((primScreenBounds.getHeight() - loginStage.getHeight()) / 2);
+        loginStage.setWidth(640);
+        loginStage.setHeight(480);
+        loginStage.setX((primScreenBounds.getWidth()/2) - 320);
+        loginStage.setY((primScreenBounds.getHeight()/2) - 240);
     }
 
     private void loadStage(Stage stage, String resourcePath, String title)
@@ -29,7 +31,9 @@ public class Main extends Application {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(resourcePath));
             AnchorPane root = fxmlLoader.load();
+            root.setId("userLogin");
             Scene scene = new Scene(root);
+            scene.getStylesheets().addAll(this.getClass().getResource("css/background.css").toExternalForm());
             stage.setScene(scene);
             stage.setTitle(title);
             stage.setResizable(false);
