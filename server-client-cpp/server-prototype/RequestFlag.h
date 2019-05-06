77138b288a1ab7503409af45220e93b7753d112d
Martin11M <martinjanczurewicz@gmail.com>
2019-05-06 20:24:30 +0200

    added response and request flags

---
+++ server-client-cpp/server-prototype/RequestFlag.h
@@ -0,0 +1,41 @@
+//
+// Created by marjan on 06.05.19.
+//
+
+#ifndef SERVER_PROTOTYPE_REQUESTFLAG_H
+#define SERVER_PROTOTYPE_REQUESTFLAG_H
+
+public enum RequestFlag {
+    LOGGING, // logowanie
+    SIGNOUT, // wylogowanie - zastanowić się, czy musimy wysyłać requesta, żeby się wylogować
+    REGISTR, // rejestracja
+
+    USERGRP, // wyświetl grupy użytkownika
+    SHOWGRP, // pokaż konkretną grupę
+    GRPLIST, // pokaż wszystkie dostepne grupy
+    MAKEGRP, // stwórz nową grupę
+    RMGROUP, // usuń grupę
+    LEAVGRP, // wyjdź z grupy
+    LEADREQ, // aplikuj o status lidera
+    MEMBREQ, // aplikuj o dolaczenie do grupy
+
+    USERREQ, // wyświetl requesty użytkowników
+    USERDEC, // zaakceptuj/odrzuć użytkownika
+
+    GRPEVNT, // wyświetl eventy grupy
+    SHOWEVT, // pokaż konkretny event
+    MAKEEVT, // stwórz nowy event
+    RMEVENT, // usuń event
+
+    EVNTAPT, // pokaż terminy eventu
+    SHOWAPT, // wybierz konkretny termin
+    MAKEAPT, // stwórz nowy termin
+    PROPAPT, // proponuj nowy termin
+    RMAPTMT, // usuń termin
+    APTMDEC, // zaakceptuj/odrzuć termin
+
+    NEWVOTE, // zagłosuj na termin
+    COMMENT  // dodaj komentarz
+}
+
+#endif //SERVER_PROTOTYPE_REQUESTFLAG_H
---
+++ server-client-cpp/server-prototype/ResposneFlag.h
@@ -0,0 +1,44 @@
+//
+// Created by marjan on 06.05.19.
+//
+
+#ifndef SERVER_PROTOTYPE_RESPOSNEFLAG_H
+#define SERVER_PROTOTYPE_RESPOSNEFLAG_H
+
+enum ResponseFlag {
+    LOGGING, // pomyślne logowanie
+    REGISTR, // pomyślna rejestracja
+
+    USERGRP, // grupy użytkownika
+    SHOWGRP, // konkretna grupa
+    GRPLIST, // wszystkie mozliwe grupy
+    MAKEGRP, // pomyślne stworzenie grupy
+    RMGROUP, // pomyślne usunięcie grupy
+    LEAVGRP, // pomyślne wyjście z grupy
+    LEADREQ, // zaaplikowano o status lidera
+    LEADRSP, // decyzja o nadaniu statusu lidera
+    MEMBREQ, // decyzja o dodaniu do grupy
+
+    USERREQ, // requesty użytkowników
+    USERDEC, // potwierdzenie decyzji
+
+    GRPEVNT, // eventy grupy
+    SHOWEVT, // konkretny event
+    MAKEEVT, // pomyślne stworzenie eventu
+    RMEVENT, // pomyślne usunięcie eventu
+
+    EVNTAPT, // terminy eventu
+    SHOWAPT, // konkretny termin
+    MAKEAPT, // pomyślne stworzenie nowego terminu
+    PROPAPT, // potwierdzenie wysłania propozycji
+    RMAPTMT, // pomyślne usunięcie terminu
+    APTMDEC, // potwierdzenie decyzji
+
+    NEWVOTE, // pomyślnie zagłosowano
+    COMMENT, // pomyślnie dodano komentarz
+
+
+    __ERROR  // błąd
+}
+
+#endif //SERVER_PROTOTYPE_RESPOSNEFLAG_H
